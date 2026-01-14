f8148062a824cf91366caad18b06c652593525ce
Marius Gabriel Lupu <wolffdev@gmail.com>
2026-01-14 16:59:59 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/flowpatch-website/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
